96a5239254c57da08ae33d624f5db0bc008af781
victor.silva <victor.silva@sankhya.com.br>
2021-04-25 13:03:24 -0300

    mudanca na estrutura de pacotes

---
+++ src/main/java/com/vhworks/hifood/domain/service/CadastroRestauranteService.java
@@ -0,0 +1,34 @@
+package com.vhworks.hifood.domain.service;
+
+import com.vhworks.hifood.domain.exception.RestauranteNaoEncontradoException;
+import com.vhworks.hifood.domain.model.Cozinha;
+import com.vhworks.hifood.domain.model.Restaurante;
+import com.vhworks.hifood.domain.repository.RestauranteRepository;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class CadastroRestauranteService {
+
+	@Autowired
+	private RestauranteRepository restauranteRepository;
+
+	@Autowired
+	private CadastroCozinhaService cozinhaService;
+
+	public Restaurante salvar(Restaurante restaurante) {
+		Long cozinhaId = restaurante.getCozinha().getId();
+
+		Cozinha cozinha = cozinhaService.buscarOuFalhar(cozinhaId);
+
+		restaurante.setCozinha(cozinha);
+
+		return restauranteRepository.save(restaurante);
+	}
+
+	public Restaurante buscarOuFalhar(Long restauranteId) {
+		return restauranteRepository.findById(restauranteId).orElseThrow(
+				() -> new RestauranteNaoEncontradoException(restauranteId));
+	}
+}
