e5b203607111823e64e6cb7985cb65be339c0990
victor.silva <victor.silva@sankhya.com.br>
2021-05-10 10:49:46 -0300

    tratado restaurantemModel

---
+++ src/main/java/com/hifood/api/model/CidadeResumoModel.java
@@ -0,0 +1,14 @@
+package com.hifood.api.model;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class CidadeResumoModel {
+
+	private Long id;
+	private String nome;
+	private String estado;
+
+}
--- src/main/java/com/hifood/core/modelmapper/ModelMapperConfig.java
+++ src/main/java/com/hifood/core/modelmapper/ModelMapperConfig.java
@@ -4,12 +4,23 @@ import org.modelmapper.ModelMapper;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import com.hifood.api.model.EnderecoModel;
+import com.hifood.domain.model.Endereco;
+
 @Configuration
 public class ModelMapperConfig {
 
 	@Bean
 	public ModelMapper modelMapper() {
-		return new ModelMapper();
+		var modelMapper = new ModelMapper();
+		
+		
+	var enderecoToEnderecoModelTypeMap = modelMapper.createTypeMap(Endereco.class, EnderecoModel.class);
+	enderecoToEnderecoModelTypeMap.<String>addMapping(
+			enderecoSrc -> enderecoSrc.getCidade().getEstado().getNome(),
+			(enderecoModelDest, value) -> enderecoModelDest.getCidade().setEstado(value));
+		
+		return modelMapper;
 	}
 
 }
